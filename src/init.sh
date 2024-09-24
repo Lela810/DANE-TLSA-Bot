7c8249cdb6bd0d19a39c8051b03e5ab25a9eb944
Lela810 <leandro.klaus03@gmail.com>
2024-09-24 22:23:58 +0000

    Sep 25, 2024, 12:23 AM

--- src/init.sh
+++ src/init.sh
@@ -3,5 +3,6 @@ echo "Starting DANE-TLSA Bot"
 sed -i "s/apiTokenReplace/$CFTOKEN/g" creds.json
 sed -i "s/accountIDReplace/$CFUID/g" creds.json
 sed -i "s/DOMAIN/$DOMAIN/g" dnsconfig.js.original
+sed -i "s/DOMAIN/$DOMAIN/g" dane-tlsa-bot.sh
 sed -i "s/MAILSUBDOMAIN/$MAILSUBDOMAIN/g" dane-tlsa-bot.sh
 sed -i "s/MAILSUBDOMAIN/$MAILSUBDOMAIN/g" dnsconfig.js.original
