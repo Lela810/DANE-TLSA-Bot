f0ac4e07cf88702b925d23cd45cd269e0440c604
Lela810 <leandro.klaus03@gmail.com>
2024-09-24 23:06:52 +0000

    Sep 25, 2024, 1:06 AM

--- src/dane-tlsa-bot.sh
+++ src/dane-tlsa-bot.sh
@@ -3,7 +3,8 @@ cd /app
 echo "Running DANE-TLSA update"
 rm dnsconfig.js sha256sum.txt certificate.pem certificate.der
 echo "Downloading certificate"
-openssl x509 -in <(openssl s_client -connect MAILSUBDOMAIN.ROOTDOMAIN:25 -starttls smtp -prexit 2>/dev/null) -out certificate.pem -outform pem
+openssl s_client -connect MAILSUBDOMAIN.ROOTDOMAIN:25 -starttls smtp -prexit </dev/null 2>/dev/null|openssl x509 -outform PEM >certificate.pem
+
 openssl x509 -in certificate.pem -pubkey -noout | openssl ec -pubin -outform der -out certificate.der
 echo "Generating SHA256 hash"
 sh -c 'sha256sum < "certificate.der" | cut -d" " -f1' -- "$file" > sha256sum.txt
