c0a874c5426ef849fc922e9842f6e7649832c09c
Lela810 <leandro.klaus03@gmail.com>
2024-09-24 22:21:16 +0000

    Sep 25, 2024, 12:21 AM

--- src/dane-tlsa-bot.sh
+++ src/dane-tlsa-bot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-openssl x509 -in <(openssl s_client -connect MAILSERVER:25 -starttls smtp -prexit 2>/dev/null) -out certificate.pem -outform pem
+rm dnsconfig.js sha256sum.txt certificate.pem certificate.der
+openssl x509 -in <(openssl s_client -connect MAILSUBDOMAIN.DOMAIN:25 -starttls smtp -prexit 2>/dev/null) -out certificate.pem -outform pem
 openssl x509 -in certificate.pem -pubkey -noout | openssl ec -pubin -outform der -out certificate.der
 sh -c 'sha256sum < "certificate.der" | cut -d" " -f1' -- "$file" > sha256sum.txt
 shaSum=`cat sha256sum.txt`
